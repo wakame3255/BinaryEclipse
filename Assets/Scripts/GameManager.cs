ec2becc7ef34be20e6486ccc2aa0e53ca13a1aa8
wakame3255 <mnanka20@gmail.com>
2024-10-25 11:16:25 +0900

    Revert "kk"
    This reverts commit 3c60444c0634eba3625b8b2f71023ff245825ea7.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(InputManager))]
-[RequireComponent(typeof(ObjectDictionary))]
 public class GameManager : MonoBehaviour
 {
-    private GameObject[] sceneGameObjects;
     void Start()
     {
         
@@ -14,21 +11,5 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
-    }
-
-    /// <summary>
-    /// コンポーネント存在確認。なかった場合はAddを行う
-    /// </summary>
-    /// <typeparam name="T">チェックの行うコンポーネント</typeparam>
-    /// <returns>コンポーネント</returns>
-    protected private T CheckComponentMissing<T>()
-    {
-        T component;
-        if (!TryGetComponent<T>(out component))
-        {
-            Debug.LogError(transform.name + " " + typeof(T).FullName + "が足りないよ");
-            gameObject.AddComponent(typeof(T));
-        }
-        return component;
     }
 }
