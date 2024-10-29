60455a4b4b6c881a3846442610dda8f8ad48aace
wakame3255 <mnanka20@gmail.com>
2024-10-29 16:45:52 +0900

    パケ追加

--- Assets/Scripts/Character/RotationMove.cs
+++ Assets/Scripts/Character/RotationMove.cs
@@ -23,14 +23,22 @@ public class RotationMove : MonoBehaviour
         DoRotationMove(pos);
     }
 
+    /// <summary>
+    /// 回転を制御するメソッド
+    /// </summary>
+    /// <param name="targetPos">ターゲットの位置</param>
     public void DoRotationMove(Vector3 targetPos)
     {
         float targetAngle = ReturnTargetToAngle(targetPos);
 
         _cacheTransform.rotation = Quaternion.AngleAxis(Mathf.Rad2Deg * targetAngle, Vector3.forward);
-
     }
 
+    /// <summary>
+    /// ターゲットに対してのxから見た角度
+    /// </summary>
+    /// <param name="targetPos">ターゲットの位置</param>
+    /// <returns>ターゲットのいる角度</returns>
     private float ReturnTargetToAngle(Vector3 targetPos)
     {
         Vector3 targetDirection = targetPos - _cacheTransform.position;
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -8,23 +8,33 @@ public class InputManager : MonoBehaviour
     private static float _inputX;
     private static float _inputY;
 
+    private static Vector3 _mousePosition;
+
     public static float InputX { get => _inputX; }
     public static float InputY { get => _inputY; }
+
+    public static Vector3 MousePosition { get => _mousePosition; }
 
     private void Update()
     {
         CheckKeyBoardDevice();
+        SetMousePosition();
     }
 
     private void CheckKeyBoardDevice()
     {
         if (Keyboard.current != null)
         {
-            SetInputInfomation();
+            SetMoveInfomation();
         }
     }
 
-    private void SetInputInfomation()
+    private void SetMousePosition()
+    {
+
+    }
+
+    private void SetMoveInfomation()
     {
         float inputX;
         float inputY;
