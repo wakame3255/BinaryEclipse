e7fa94ef8c268103d5fa49dcfa0f7ea9012ac6f4
wakame3255 <mnanka20@gmail.com>
2024-11-14 16:47:54 +0900

    足ロイロ追加

---
+++ Assets/Scripts/Character/BaseCharacterStateView.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using R3;
+
+public class BaseCharacterStateView : MonoBehaviour
+{
+    private CharacterStatus characterStatus;
+
+    protected void Start()
+    {
+        
+    }
+}
--- Assets/Scripts/Character/CharacterStatus.cs
+++ Assets/Scripts/Character/CharacterStatus.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using R3;
 
 public class CharacterStatus : MonoBehaviour
 {
     [SerializeField]
     private int _hp;
+
+    private Subject<int> _hpSubject = new Subject<int>();
 
     public int Hp { get => _hp;}
 
