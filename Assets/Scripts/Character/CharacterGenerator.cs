8a744df0ac3efdda00f0b3af863a886e9e120bf5
wakame3255 <mnanka20@gmail.com>
2024-11-19 10:46:59 +0900

    d

---
+++ Assets/Scripts/Character/CharacterGenerator.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterGenerator : MonoBehaviour
+{
+    private StateMachineGenerator _stateMachineGenerator;
+    private CharacterStateDictionary _characterStateDictionary;
+
+    public void GenerateCharacter()
+    {
+
+    }
+}
---
+++ Assets/Scripts/LosicState/StateMachineGenerator.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachineGenerator : MonoBehaviour
+{
+    public void InitializeStateMachine(ICpuCharacter[] cpuCharacters)
+    {
+
+    }
+}
