43e19903ade7d6f2140d729bc940f0fc937efe16
wakame3255 <46024199+wakame3255@users.noreply.github.com>
2025-02-05 16:00:02 +0900

    コメント付け

--- Assets/Scripts/LosicState/HpCheckStateNode.cs
+++ Assets/Scripts/LosicState/HpCheckStateNode.cs
@@ -2,52 +2,74 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// HPチェック用のステートノードクラス
+/// </summary>
 public class HpCheckStateNode : BaseStateNode
 {
     [SerializeField]
-    private OutNode _falseNode;
+    private OutNode _falseNode; // HPチェックが失敗した場合の遷移先ノード
 
     [SerializeField]
-    private int _hp;
+    private int _hp; // チェック対象のHP値
 
-    private bool _existsCharacter = default;
+    private bool _existsCharacter = default; // 指定されたHP以下のキャラクターが存在するかどうかのフラグ
+
+    /// <summary>
+    /// ステートに入る際の処理
+    /// </summary>
     public override void EnterState()
     {
+        // 同盟キャラクターをループしてHPが指定値以下かを確認
         foreach (CharacterStateView characterState in _otherCharacters.Allys)
         {
             if (characterState.Hp <= _hp)
             {
                 _existsCharacter = true;
-                break;
+                break; // 条件を満たすキャラクターが見つかったためループを抜ける
             }
             else
             {
                 _existsCharacter = false;
             }
         }
-        base.EnterState();
+        base.EnterState(); // 基底クラスのEnterStateを呼び出す
     }
+
+    /// <summary>
+    /// ステートの更新処理
+    /// </summary>
     public override void UpdateState()
     {
         if (_existsCharacter)
         {
+            // 条件を満たすキャラクターが存在する場合、通常の遷移先に遷移
             _cpuCharacter.StateMachine.TransitionNextState(_outNode.NextStateNode);
         }
         else
         {
+            // 条件を満たすキャラクターが存在しない場合、別の遷移先に遷移
             _cpuCharacter.StateMachine.TransitionNextState(_falseNode.NextStateNode);
         }
     }
+
+    /// <summary>
+    /// ステートから退出する際の処理
+    /// </summary>
     public override void ExitState()
     {
-        base.ExitState();
+        base.ExitState(); // 基底クラスのExitStateを呼び出す
     }
 
+    /// <summary>
+    /// このステートノードが持つ出力ノードを取得
+    /// </summary>
+    /// <returns>出力ノードの配列</returns>
     public override OutNode[] GetHasOutNode()
     {
-        _outNode.UpdateNextNode();
-        _falseNode.UpdateNextNode();
+        _outNode.UpdateNextNode(); // 通常の出力ノードを更新
+        _falseNode.UpdateNextNode(); // 失敗時の出力ノードを更新
 
-        return new OutNode[] { _outNode, _falseNode };
+        return new OutNode[] { _outNode, _falseNode }; // 出力ノードを配列で返す
     }
 }
--- Assets/Scripts/LosicState/StateMachine.cs
+++ Assets/Scripts/LosicState/StateMachine.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+/// <summary>
+/// ステートマシンに必要な情報を保持するクラス
+/// </summary>
 public class StateMachineInformation
-{   
+{
     public ICpuCharacter CpuCharacterDate { get; private set; }
     public CpuController CpuControllerDate { get; private set; }
     public StartStateNode StartStateNodeDate { get; private set; }
     public OtherCharacterStatus OtherCharacterStatusDate { get; private set; }
 
+    /// <summary>
+    /// コンストラクタで各情報を初期化
+    /// </summary>
+    /// <param name="cpuCharacter">CPUキャラクターのデータ</param>
+    /// <param name="cpuController">CPUコントローラー</param>
+    /// <param name="startNode">開始ステートノード</param>
+    /// <param name="otherCharacterStatus">他キャラクターのステータス</param>
     public StateMachineInformation(ICpuCharacter cpuCharacter, CpuController cpuController, StartStateNode startNode, OtherCharacterStatus otherCharacterStatus)
     {
         CpuCharacterDate = cpuCharacter;
@@ -35,30 +45,36 @@ namespace Cpu
 
         private StateMachineInformation _stateMachineInformation;
 
+        /// <summary>
+        /// 現在のステートノードを取得
+        /// </summary>
         public BaseStateNode CurrentStateNode { get; private set; }
 
         /// <summary>
-        /// ステートの開始処理
+        /// ステートマシンの初期化処理
         /// </summary>
-        /// <param name="baseState">ステート</param>
         public void Initialize()
         {
+            // ステートノードの更新を行う
             UpdateStateNode();
+            // 開始ステートノードを現在のステートに設定
             CurrentStateNode = _startStateNode;
             if (CurrentStateNode != null)
             {
+                // 開始ステートに入る
                 CurrentStateNode.EnterState();
             }
         }
 
         /// <summary>
-        /// ステートの切り替え処理
+        /// 次のステートへの遷移処理
         /// </summary>
-        /// <param name="nextStateNode">ステート</param>
+        /// <param name="nextStateNode">遷移先のステートノード</param>
         public void TransitionNextState(BaseStateNode nextStateNode)
         {
             BaseStateNode baseStateNode;
 
+            // 次のステートが指定されていない場合、ベースステートリストの最初を使用
             if (nextStateNode == null)
             {
                 baseStateNode = _baseStateNodes[0];
@@ -67,30 +83,37 @@ namespace Cpu
             {
                 baseStateNode = nextStateNode;
             }
+
+            // 現在のステートから退出
             CurrentStateNode.ExitState();
+            // 新しいステートに切り替え
             CurrentStateNode = baseStateNode;
+            // 新しいステートに入る
             CurrentStateNode.EnterState();
         }
 
         /// <summary>
-        /// ステートのUpdate処理
+        /// ステートの更新処理を実行
         /// </summary>
         public void UpdateState()
         {
             if (CurrentStateNode != null)
             {
+                // 現在のステートの更新処理を呼び出す
                 CurrentStateNode.UpdateState();
             }
         }
 
         /// <summary>
-        /// ステートマシンの生成
+        /// ステートマシンのコンストラクタ
         /// </summary>
-        /// <param name="stateMachineInformation">キャラクター情報</param>
+        /// <param name="stateMachineInformation">ステートマシンに必要なキャラクター情報</param>
         public StateMachine(StateMachineInformation stateMachineInformation)
         {
+            // 引数がnullでないことを確認
             MyExtensionClass.CheckArgumentNull(stateMachineInformation, nameof(stateMachineInformation));
 
+            // 各フィールドを初期化
             _cpuCharacter = stateMachineInformation.CpuCharacterDate;
             _cpuController = stateMachineInformation.CpuControllerDate;
             _startStateNode = stateMachineInformation.StartStateNodeDate;
@@ -98,24 +121,27 @@ namespace Cpu
 
             if (_startStateNode != null)
             {
+                // 開始ステートノードにキャラクター情報とコントローラーを設定
                 _startStateNode.SetCharacterInformation(_cpuCharacter);
                 _startStateNode.SetCpuContoller(_cpuController);
             }
-           
         }
 
         /// <summary>
-        /// ステートの流れ更新
+        /// ステートのフローを更新する
         /// </summary>
         public void UpdateStateNode()
         {
             if (CurrentStateNode != null)
             {
+                // 現在のステートから退出
                 CurrentStateNode.ExitState();
             }
 
+            // ステートノードのフローをリセットおよび更新
             UpDateStateNodeFlow();
 
+            // 各ベースステートノードにキャラクター情報を設定
             foreach (BaseStateNode baseState in _baseStateNodes)
             {
                 baseState.SetCharacterInformation(_cpuCharacter);
@@ -125,54 +151,62 @@ namespace Cpu
         }
 
         /// <summary>
-        /// ステートリセットと更新メソッド
+        /// ステートノードのフローをリセットし、再構築するメソッド
         /// </summary>
         private void UpDateStateNodeFlow()
         {
             if (_startStateNode != null)
             {
+                // ベースステートノードリストをクリア
                 _baseStateNodes.Clear();
+                // 開始ステートノードから出力ノードを取得
                 _startStateNode.GetHasOutNode();
                 OutNode[] outNodes = new OutNode[] { _startStateNode.OutNode };
+                // 再帰的に次のステートをキャッシュ
                 CacheNextState(outNodes);
             }
         }
 
         /// <summary>
-        /// ステートの連なりを再帰的に確認
+        /// ステートの連鎖を再帰的に確認し、キャッシュに追加するメソッド
         /// </summary>
-        /// <param name="outNode"></param>
+        /// <param name="outNodes">現在のステートからの出力ノード配列</param>
         private void CacheNextState(OutNode[] outNodes)
         {
+            // 引数がnullでないことを確認
             MyExtensionClass.CheckArgumentNull(outNodes, nameof(outNodes));
 
             foreach (OutNode outNode in outNodes)
             {
+                // 次のノードを更新
                 outNode.UpdateNextNode();
                 BaseStateNode nextStateNode = outNode.NextStateNode;
 
+                // 次のステートノードが有効で、ループしていない場合
                 if (nextStateNode != null && !CheckStateLoop(_baseStateNodes, nextStateNode))
                 {
+                    // ベースステートノードリストに追加
                     _baseStateNodes.Add(nextStateNode);
 
+                    // 再帰的に次のステートノードをキャッシュ
                     CacheNextState(nextStateNode.GetHasOutNode());
                 }
             }
         }
 
         /// <summary>
-        /// ステートのループチェック
+        /// ステートのループをチェックするメソッド
         /// </summary>
-        /// <param name="cacheBaseStates">キャッシュステート群</param>
-        /// <param name="baseState">追加予定のステート</param>
-        /// <returns>ループしているかのBool</returns>
+        /// <param name="cacheBaseStates">既存のキャッシュされたステートノードリスト</param>
+        /// <param name="baseState">追加予定のステートノード</param>
+        /// <returns>ループが検出された場合はtrue、それ以外はfalse</returns>
         private bool CheckStateLoop(List<BaseStateNode> cacheBaseStates, BaseStateNode baseState)
         {
             foreach (BaseStateNode stateNode in cacheBaseStates)
             {
                 if (stateNode == baseState)
                 {
-                    Debug.Log("ループ検知");
+                    // ループが検出された場合
                     return true;
                 }
             }
